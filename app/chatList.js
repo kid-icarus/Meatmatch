75941d573fd492907e6ea42e3de68cd57851bfd5
Ryan Kois <ryan.kois@gmail.com>
2013-12-08 22:37:54 -0700

    Basic ChatList Class

--- app/chatList.js
+++ app/chatList.js
@@ -1,4 +1,17 @@
 var ChatList = function() {
+  this.chats = [];
 };
+
+ChatList.prototype.push = function(chat) {
+  this.chats.push(chat);
+  if (this.chats.length > 200) {
+    this.chats.shift();
+  }
+}
+
+
+ChatList.prototype.pop = function() {
+  return this.chats.pop();
+}
 
 module.exports = ChatList;
--- test/chatList.js
+++ test/chatList.js
@@ -2,5 +2,46 @@ var ChatList = require('../app/chatList');
 var should = require('chai').should();
 
 describe('Chat List', function() {
-  it('needs specs');
+  beforeEach(function() {
+    this.chatList = new ChatList();
+  });
+
+  describe('#push', function() {
+    beforeEach(function() {
+      this.chatList.push({'first': true});
+    });
+
+    it('should push a message into the chats array', function() {
+      this.chatList.chats.should.have.length(1);
+    });
+
+    it('should pop the oldest chat off after 200 chats', function() {
+      for(var i = 0; i < 200; i++) {
+        this.chatList.push({});
+      }
+      this.chatList.chats[0].should.not.have.property('first');
+    });
+
+  });
+
+  describe('#pop', function() {
+    beforeEach(function() {
+      this.chatList.push({});
+      this.chat = this.chatList.pop();
+    });
+
+    it('should return an object', function(){
+      this.chat.should.be.an('object');
+    });
+
+    it('should pop off the chats array', function() {
+      this.chatList.chats.should.have.length(0);
+    });
+
+    it('should return undefined if chatList is empty', function() {
+      should.not.exist(this.chatList.pop());
+    });
+
+  });
 });
+
